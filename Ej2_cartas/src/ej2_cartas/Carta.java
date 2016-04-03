00a1f836761ad530745ddd1eefb3a8c84f1826b4
magelac94 <magelac94@gmail.com>
2016-04-03 13:30:54 -0300

    agrego cartas

---
+++ Ej2_cartas/src/ej2_cartas/Carta.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ej2_cartas;
+
+/**
+ *
+ * @author Magela
+ */
+public class Carta {
+    
+    public void palo (){
+        
+    }
+    public void numero (){
+        
+    }
+}

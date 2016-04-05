c6aec709d5cd0d4e7c17ef886c72a43f0b65aad5
magelac94 <magelac94@gmail.com>
2016-04-05 00:18:27 -0300

    Ejercicio2 de las cartas terminado

---
+++ UT2_PD2_Ej1/src/ut2_pd2_ej1/UT2_PD2_Ej1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ut2_pd2_ej1;
+
+/**
+ *
+ * @author Magela
+ */
+public class UT2_PD2_Ej1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public enum Day {
+        SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+    }
+    
+    public enum Planet {
+        MERCURIO, VENUS, TIERRA, MARTE, JUPITER, SATURNO, URANO, NEPTUNO, PLUTON
+    } 
+            
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

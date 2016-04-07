439d191ee56c8dc7cc63a4508a96c842b3714969
magelac94 <magelac94@gmail.com>
2016-04-07 00:16:17 -0300

    Ejercicio 1 repartido 5 terminado

---
+++ UT2_PD5/src/EJ1/Bucles.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EJ1;
+
+/**
+ *
+ * @author Magela
+ */
+public class Bucles {
+
+    public static void Bucless(int i,int h) {
+        int j = h;
+        while (i > 0){
+            while (j > 0){
+                System.out.print(" #");
+                j--;
+            }
+            System.out.println(" ");
+            i--;
+            j=h;
+                    
+        }
+    }
+}
---
+++ UT2_PD5/src/EJ1/Principal.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EJ1;
+
+
+/**
+ *
+ * @author Magela
+ */
+public class Principal {
+    public static void main(String[] arg){
+      // Bucles bucle = new Bucles(4,4); 
+      Bucles.Bucless(4, 4);
+    }
+    
+}

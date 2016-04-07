c5662d1579cb40a02cc4bf83033ba6c3805af34b
magelac94 <magelac94@gmail.com>
2016-04-07 17:14:21 -0300

    Ejercicio 4 rep 5
    ejercicio 4 rep 5

--- UT2_PD5/src/EJ2/Archivos.java
+++ UT2_PD5/src/EJ2/Archivos.java
@@ -49,13 +49,13 @@ public class Archivos {
         return listaLineasArchivo.toArray(new String[0]);
     }
 
-    public void escribir(int par1, float par2, String par3) {
+    public static void escribir(int par1, float par2, String par3) {
         System.out.println("El entero leido es: " + par1);
         System.out.println("El numero de punto flotante es: " + par2);
         System.out.println("La cadena leida es " + par3);
         System.out.println("!Hola " + par3 + " La suma de " + par1 + " y " + par2 + " es " + par1 + par2);
-        System.out.println("La division entera de "+par2+" y "+par1+" es "+par2/par1+" y su resto es "+par2%part1);
-        
+        //   System.out.println("La division entera de "+par2+" y "+par1+" es "+par2/par1+" y su resto es "+(par2%part1));
+
     }
 
     public static void main(String[] arg) {
@@ -63,11 +63,17 @@ public class Archivos {
         float parametro2;
         String parametro3;
         String[] lineas = leerArchivo("src/EJ2/entrada.txt");
+
         parametro1 = Integer.valueOf(lineas[0]);
         parametro2 = Float.valueOf(lineas[1]);
-        parametro3 = String.valueOf(lineas[2]);
-        escribir(1,1,"hola");
+        parametro3 = lineas[2];
         escribir(parametro1, parametro2, parametro3);
+
+        // Parte 2 -- 
+        String[] cir = leerArchivo("src/EJ2/radio.txt");
+        float radio = Float.valueOf(cir[0]);
+        System.out.println("Area: " + (Math.PI * radio * radio) / 2);
+        System.out.println("Perimetro: " + (radio + radio) * Math.PI);
 
     }
 }
